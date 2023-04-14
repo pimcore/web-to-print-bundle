577b825044b9b7c00bce89e97d184c08b3398bfe
JiaJiaJi <kingjia90@gmail.com>
2023-04-14 15:03:25 +0200

    fix missing app files

--- .github/ci/scripts/setup-environment.sh
+++ .github/ci/scripts/setup-environment.sh
@@ -3,11 +3,11 @@
 set -eu
 
 cp -r .github/ci/files/config config
-#cp -r .github/ci/files/templates templates
-#cp -r .github/ci/files/bin bin
-#chmod 755 bin/console
+cp -r .github/ci/files/templates templates
+cp -r .github/ci/files/bin bin
+chmod 755 bin/console
 cp -r .github/ci/files/kernel kernel
-#cp -r .github/ci/files/public public
+cp -r .github/ci/files/public public
 #cp -r .github/ci/files/var var
 
-#cp .github/ci/files/.env ./
+cp .github/ci/files/.env ./
