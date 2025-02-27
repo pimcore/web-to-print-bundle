5b3abd86b848f8f848be3a6a97a766b88910da6b
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2025-02-27 10:34:23 +0100

    [Task] Add container extension (#96)
    * [Task] Add container extension
    
    * Remove chromium

--- src/PimcoreWebToPrintBundle.php
+++ src/PimcoreWebToPrintBundle.php
@@ -16,15 +16,22 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\WebToPrintBundle;
 
+use Pimcore\Bundle\WebToPrintBundle\DependencyInjection\PimcoreWebToPrintExtension;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
 use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class PimcoreWebToPrintBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
     use BundleAdminClassicTrait;
     use PackageVersionTrait;
+
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new PimcoreWebToPrintExtension();
+    }
 
     public function getCssPaths(): array
     {
