a8de0187c862a8e3c0add3b24f5f98345c7bd89d
Herbert Roth <herbert.roth@pimcore.com>
2025-04-22 15:36:03 +0200

    Add debug output for temp file metadata and path

--- tests/Model/Processor/ProcessorTest.php
+++ tests/Model/Processor/ProcessorTest.php
@@ -72,9 +72,10 @@ class ProcessorTest extends ModelTestCase
         $tempMetadata = stream_get_meta_data($file);
         $tempPath = $tempMetadata['uri'];
         file_put_contents($tempPath, $pdfContent);
-
+        var_dump($tempMetadata);
         $gs = new Ghostscript();
         $pdfText = $gs->getText(null, null, $tempPath);
+        var_dump($tempPath);
         $this->assertStringContainsString('Pellentesque habitant morbi tristiqu', $pdfText, 'Check if pdf contains text from html template');
 
         $pdfInfo = $this->getPDFInfo($tempPath);
