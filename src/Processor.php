6208d85eb95fa7a654904ac7c44de377f8998ffd
Herbert Roth <herbert.roth@pimcore.com>
2025-10-16 09:55:42 +0000

    Deprecate retrieval of templating engine from container in Processor class

--- src/Processor.php
+++ src/Processor.php
@@ -240,6 +240,7 @@ abstract class Processor
     {
         $document = $params['document'] ?? null;
         $hostUrl = $params['hostUrl'] ?? null;
+        trigger_deprecation('pimcore/pimcore', '12.3', 'Retrieving "pimcore.templating.engine.delegating" from the container is deprecated and will be removed in 13.0. Inject Twig\Environment directly instead.');
         $templatingEngine = \Pimcore::getContainer()->get('pimcore.templating.engine.delegating');
 
         try {
